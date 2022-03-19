7256c4ef222b50f7b4297a6f985216990430f785
mirecki <michal.mirecki95@gmail.com>
2022-03-19 21:58:48 +0100

    misssing factories

--- spec/factories/access_tokens.rb
+++ spec/factories/access_tokens.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :access_token, class: Doorkeeper::AccessToken do
+  factory :access_token, class: 'Doorkeeper::AccessToken' do
     application       { create(:oauth_application) }
     resource_owner_id { create(:user).id }
   end
--- spec/factories/oauth_applications.rb
+++ spec/factories/oauth_applications.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :oauth_application, class: Doorkeeper::Application do
+  factory :oauth_application, class: 'Doorkeeper::Application' do
     name          { 'Test' }
     redirect_uri  { 'urn:ietf:wg:oauth:2.0:oob' }
     uid           { '12345678' }
