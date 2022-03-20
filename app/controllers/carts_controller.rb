61186227a5f1be747521c1c7e04ff2d6b69fb9a8
mirecki <michal.mirecki95@gmail.com>
2022-03-20 13:47:12 +0100

    Rubocop

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class CartsController < ApplicationController
-  
   def destroy
     current_cart.destroy!
   end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,5 @@ class MainController < ApplicationController
   def index
     @studios = Studio.limit(3)
     @hits = Film.active.hit.limit(8)
-
   end
 end
